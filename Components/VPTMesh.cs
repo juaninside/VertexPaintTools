e5f6a381daf2098b9d8e8e1d85a1e1445c21e119
Juan <juaninpolancolopez@gmail.com>
2020-11-09 22:52:28 +0100

    avoid using .mesh, using .sharedMesh instead to create a duplicated mesh

---
+++ Components/VPTMesh.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(MeshCollider))]
+public class VPTMesh : MonoBehaviour
+{
+    private Mesh _mesh;
+    [HideInInspector]
+    public Mesh Mesh
+    {
+        get
+        {
+            if (_mesh == null)
+                GetComponent<MeshFilter>().sharedMesh = _mesh = GetUniqueMesh();
+            return _mesh;
+        }
+    }
+
+    Mesh GetUniqueMesh()
+    {
+        var newMesh = new Mesh();
+        var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+        newMesh.vertices = originalMesh.vertices;
+        newMesh.triangles = originalMesh.triangles;
+        newMesh.uv = originalMesh.uv;
+        newMesh.normals = originalMesh.normals;
+        newMesh.colors = originalMesh.colors;
+        newMesh.tangents = originalMesh.tangents;
+        newMesh.name = originalMesh.name + " VPT" + gameObject.GetInstanceID();
+
+        return newMesh;
+    }
+
+}
